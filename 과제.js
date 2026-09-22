75697c1de290a181581ab05f80aeabfd3bf5c41a
howolcat <pbsokok7@gmail.com>
2026-09-22 09:36:41 +0900

    Refactor button event listener variable name

--- 과제.js
+++ 과제.js
@@ -1,6 +1,6 @@
-document.querySelectorAll('.category button').forEach((btn, i) => {
+document.querySelectorAll('.category button').forEach((x, i) => {
     const ids = ['c1','c2','c3','c4','c5','c6'];
-    btn.addEventListener('click', () => {
+    x.addEventListener('click', () => {
         document.getElementById(ids[i]).scrollIntoView({behavior: 'smooth'});
     });
 });
